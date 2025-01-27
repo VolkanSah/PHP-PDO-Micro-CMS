fa85e47fc6f2950855b3a8b622ad32707164d0b5
Volkan Şah <VolkanSah@users.noreply.github.com>
2025-01-27 13:35:02 +0100

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE micro_cms;
+
+USE micro_cms;
+
+CREATE TABLE pages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
